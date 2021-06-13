57fad014a16c9ab5b3a4e8999b98f14e980e5d21
sumo101 <symonette@knights.ucf.edu>
2021-06-13 13:25:28 -0400

    Initial solution to problem 29

---
+++ src/main/java/ex29/App.java
@@ -0,0 +1,29 @@
+package ex29;
+
+
+import java.util.Scanner;
+
+
+public class App {
+    public static void main(String[] args){
+        boolean running = true;
+        Scanner inputScanner = new Scanner(System.in);
+        while (running){
+            try{
+                System.out.print("What is the rate of return? ");
+                String rateOfReturnStr = inputScanner.nextLine();
+                double rateOfReturn = Double.parseDouble(rateOfReturnStr);
+                if (rateOfReturn == 0.){
+                    System.out.print("Zero (0) is not a valid rate of return! ");
+                    continue;
+                }
+                double estimatedYears = 72 / rateOfReturn;
+                System.out.println("It will take " +estimatedYears + "to double your initial investment.");
+                running = false;
+            }catch (Exception e){
+                System.out.println("Sorry. That is not the valid input,");
+            }
+        }
+        inputScanner.close();
+    }
+}
